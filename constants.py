61367696a5eec3ba601ad3bfdd77b0e577395491
Paul Tan <pjxtan@gmail.com>
2014-09-14 18:44:37 -0400

    Physical constants

---
+++ constants.py
@@ -0,0 +1,39 @@
+"""
+Physical constants (in SI units).
+"""
+
+# Vacuum permittivity [F m^{-1}]
+eps_0 = 8.854187817e-12
+
+# Planck constant [J s]
+h = 6.62606957e-34
+
+# Reduced Planck constant [J s]
+hbar = 1.054571726e-34
+
+# Boltzmann constant [J K^{-1}]
+k_B = 1.3806488e-23
+
+# Speed of light [m s^{-1}]
+c = 2.99792458e8
+
+# Gravitational constant [m^3 kg^{-1} s^{-2}]
+G = 6.67384e-11
+
+# WGS gravitational acceleration on Earth [m s^{-2}]
+g = 9.80665
+
+# Solar mass [kg]
+M_sol = 1.9891e30
+
+# Solar radius [m]
+R_sol = 6.955e8
+
+# Electron charge [C] (or electron-volt [J])
+e = 1.60217657e-19
+
+# Electron mass [kg]
+m_e = 9.10938291e-31
+
+# Hydrogen mass [kg]
+m_H = 1.673532499e-27
