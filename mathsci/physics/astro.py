f6a367bcf5a568f38f49313636b062be9627ea6a
Paul J.X. Tan <pjxtan@gmail.com>
2014-09-18 12:24:10 -0400

    Added structure equations

--- mathsci/physics/astro.py
+++ mathsci/physics/astro.py
@@ -34,3 +34,40 @@ def orbit_1body(t, X, **kwargs):
 
     X_dot = [x_dot, y_dot, vx_dot, vy_dot]
     return mathsci.datatypes.Vector(X_dot)
+
+def stellar_structure(r, X, **kwargs):
+    """
+    Solves the stellar structure equations of hydrostatic equilibrium and
+    mass conservation.
+
+    @type  r: number
+    @param r: distance from center [m]
+    @type  X: vector
+    @param X: X[0] = M = enclosed mass at radius r [kg];
+              X[1] = rho = density at radius r [kg m^{-3}]
+
+    @rtype: vector
+    @return: evolved state vector
+    """
+    M = X[0]
+    rho = X[1]
+
+    # Get kwargs
+    if len(kwargs) != 3:
+        raise ValueError("Bad kwargs; please provide all of the "\
+                         "following parameters: rho, mu, T")
+
+    # Hydrostatic equilibrium
+    dP_dr = - mathsci.constants.G * M / r**2 * kwargs['rho']
+    
+    # Pressure
+    P, dP_drho = ideal_gas_pressure(kwargs['rho'], kwargs['mu'], kwargs['T'])
+    drho_dP = 1.0 / dP_drho
+    drho_dr = drho_dP * dP_dr
+    
+    # Mass conservation
+    dM_dr = 4 * math.pi * r**2 * kwargs['rho']
+    
+    X_dot = [dM_dr, drho_dr]
+    
+    return mathsci.datatypes.Vector(X_dot)
