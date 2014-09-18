4a9c221a69ccfeeac7ab7f5337c423aac6510564
Paul J.X. Tan <pjxtan@gmail.com>
2014-09-18 11:55:29 -0400

    Electrostatics

---
+++ mathsci/physics/elecstat.py
@@ -0,0 +1,39 @@
+"""
+Electrostatics.
+"""
+
+import math
+import mathsci.constants
+
+def potential(q, r):
+    """
+    Returns the electric potential at distance r from a point charge q.
+
+    @type  q: number
+    @param q: source charge [C]
+    @type  r: number
+    @param r: distance from source [m]
+
+    @rtype: number
+    @return: electric potential at r [V]
+    """
+    q = float(q)
+    r = float(r)
+    return q / 4 / math.pi / mathsci.constants.eps_0 / r
+
+def field(q, r):
+    """
+    Returns the magnitude of the electric field at distance r from a point
+    charge q.
+
+    @type  q: number
+    @param q: source charge [C]
+    @type  r: number
+    @param r: distance from source [m]
+
+    @rtype: number
+    @return: electric field at r [N/C]
+    """
+    q = float(q)
+    r = float(r)
+    return q / 4 / math.pi / mathsci.constants.eps_0 / r**2
