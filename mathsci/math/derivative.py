2b2c748cd540179f9ca945133cbd91047a803c72
Paul Tan <pjxtan@gmail.com>
2014-09-17 20:23:37 -0400

    Evaluating derivatives

---
+++ mathsci/math/derivative.py
@@ -0,0 +1,137 @@
+"""
+Evaluating derivatives.
+"""
+
+# TODO:
+#     Include error estimates for each method?
+
+def forward_difference(f, x, h):
+    """
+    Evaluates the first derivative at x, with step size h, using a forward
+    difference scheme.
+
+    @type  f: function
+    @param f: function to differentiate
+    @type  x: number
+    @param x: position at which to evaluate
+    @type  h: number
+    @param h: step size
+
+    @rtype: number
+    @return: first derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    D = (f(x + h) - f(x)) / h
+    return D
+
+def backward_difference(f, x, h):
+    """
+    Evaluates the first derivative at x, with step size h, using a backward
+    difference scheme.
+
+    @type  f: function
+    @param f: function to differentiate
+    @type  x: number
+    @param x: position at which to evaluate
+    @type  h: number
+    @param h: step size
+
+    @rtype: number
+    @return: first derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    D = (f(x) - f(x - h)) / h
+    return D
+
+def central_difference(f, x, h):
+    """
+    Evaluates the first derivative at x, with step size h, using a central
+    difference scheme (of degree 1).
+
+    @type  f: function
+    @param f: function to differentiate
+    @type  x: number
+    @param x: position at which to evaluate
+    @type  h: number
+    @param h: step size
+
+    @rtype: number
+    @return: first derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    D = (f(x + h / 2.0) - f(x - h / 2.0)) / h
+    return D
+
+def central_difference_degree(f, x, h, deg):
+    """
+    Evaluates the first derivative at x, with step size h, using a central
+    difference scheme (of degree deg).
+
+    @type    f: function
+    @param   f: function to differentiate
+    @type    x: number
+    @param   x: position at which to evaluate
+    @type    h: number
+    @param   h: step size
+    @type  deg: number
+    @param deg: degree of central difference scheme
+
+    @rtype: number
+    @return: first derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    deg = int(deg)
+    return
+
+def central_difference_second(f, x, h):
+    """
+    Evaluates the second derivative at x, with step size h, using a central
+    difference scheme (of degree 1).
+
+    @type  f: function
+    @param f: function to differentiate
+    @type  x: number
+    @param x: position at which to evaluate
+    @type  h: number
+    @param h: step size
+
+    @rtype: number
+    @return: second derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    D = (central_difference(f, x + h / 2, h)
+         - central_difference(f, x - h / 2, h)) / h
+    return D
+
+def central_difference_order(f, x, h, n):
+    """
+    Evaluates the n-th derivative at x, with step size h, using a central
+    difference scheme (of degree 1).
+
+    @type  f: function
+    @param f: function to differentiate
+    @type  x: number
+    @param x: position at which to evaluate
+    @type  h: number
+    @param h: step size
+    @type  n: number
+    @param n: degree of derivative
+
+    @rtype: number
+    @return: n-th derivative of f evaluated at x
+    """
+    x = float(x)
+    h = float(h)
+    n = int(n)
+    return
+
+def partial():
+    return
+
+if __name__ == "__main__":
+    pass
