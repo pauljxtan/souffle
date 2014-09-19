a4ff3b5675167f39ac1a98f0ba553b0792cff572
Paul J.X. Tan <pjxtan@gmail.com>
2014-09-19 11:34:01 -0400

    Computing trig functions

---
+++ examples/trig_funcs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+Computing various trigonometric functions by solving nonlinear equations.
+"""
+
+import math
+from mathsci.math import nonlineq
+
+def arctanh(x):
+    """
+    Computes the inverse hyperbolic tangent using Newton's method.
+    """
+    f = lambda y: math.tanh(y) - x
+    f_deriv = lambda y: math.cosh(y)**(-2)
+    x, n_iter = nonlineq.newton(f, f_deriv, 0.0, 1e-12)
+
+    return x
+
+if __name__ == "__main__":
+    print "arctanh(0.5) =", arctanh(0.5)
+
