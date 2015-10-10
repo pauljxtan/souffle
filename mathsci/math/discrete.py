bd63eafcc35744dfc953ad69a0c9cdfe85ba3b7c
Paul J.X. Tan <pjxtan@gmail.com>
2015-10-09 21:31:30 -0400

    Improved factorial(); added more documentation for binomial_coffecient()

--- mathsci/math/discrete.py
+++ mathsci/math/discrete.py
@@ -13,14 +13,20 @@ def factorial(n):
     """
     n = int(n)
 
-    if n > 1:
-        return n * factorial(n - 1)
-    else:
+    if n < 0:
+        raise ValueError("This factorial function is undefined for negative "
+                         "numbers")
+    if n == 1:
         return 1
+    return n * factorial(n - 1)
 
 def binomial_coefficient(n, k):
     """
-    Computes "n choose k".
+    Computes the coefficient of the x^k term in the binomial expansion of
+    (1+x)^n. This quantity is also referred to as "n choose k" in
+    combinatorics, that is, the number of ways to select k elements from a
+    set of n elements. The family of binomial coefficients form Pascal's
+    triangle, by way of Pascal's rule.
 
     @type n: number
     @type k: number
